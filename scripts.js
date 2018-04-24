70317dd1b85e0cd8765248832462026402478048
Luxapodular <luca.damasco@gmail.com>
2018-04-23 21:09:07 -0400

    Added getItem function.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,14 @@
 // Change the "url" value to the url of your web app;
 APP_URL = "<YOUR_APP_URL>";
 
-function sendObj(obj) {
-    var response = $.ajax(
-    {
-      url: APP_URL, 
-      method: "GET",
-      dataType: "json",
-      data: obj,
-    }).success();
-}
+// Post data to the google sheet. You can edit this function to send different data! 
 
-function postToSheet(sheet, act, val) {
+// act : String | Action you want to store
+// val : String | Data you want to store 
+// sheet : String | The sub sheet to post to
+function postToSheet(act, val, sheet) {
   
+  // NOTE: The keys in your payload must have headers on the sheet you are sending to!
   payload = {
     timestamp: new Date().getTime(),
     action: act,
@@ -24,10 +20,16 @@ function postToSheet(sheet, act, val) {
   sendObj(payload); 
 }
 
-function getColumn(headerName, callback) {
-  obj = {
+// Returns all elements of a column that is headed by "headerName" as an array.
+
+// headerName : String | Header name of the column you want. Must be in the first row of the sheet.
+// callback : function | function to send array to when returned.
+// sheet : String | The sub sheet to post to
+function getColumn(headerName, callback, sheet) {
+  payload = {
     getColumn: true,
     headerName: headerName, 
+    sheetName: sheet,
   }
   
   // If no callback supplied, just log the result. 
@@ -45,6 +47,45 @@ function getColumn(headerName, callback) {
     }).success();
 }
 
+// Returns the alement at a specific cell using A1 notation. Ex. TO get the item in row 3 column c, call getItem("3", "c")
+
+// row : String | row of item to retrieve. Should be a number.
+// col : String | column of item to retrieve. Should be a letter.
+// callback : function | function to send array to when returned.
+// sheet : String | The sub sheet to post to
+function getItem(row, col, callback, sheet) {
+  
+  payload = {
+    getItem: true,
+    cell: col+row,
+    sheetName: sheet,
+  }
+  
+  // If no callback supplied, just log the result. 
+  if (callback === undefined) {
+    callback = console.log; 
+  }
+  
+  response = $.ajax(
+    {
+      url: APP_URL, 
+      method: "GET",
+      dataType: "json",
+      data: obj,
+      success: function(response) { callback(response.cell) },
+    }).success();
+}
+
 function runColumnCallback(response, callback) {
   callback(response.column); 
+}
+
+function sendObj(obj) {
+    var response = $.ajax(
+    {
+      url: APP_URL, 
+      method: "GET",
+      dataType: "json",
+      data: obj,
+    }).success();
 }
